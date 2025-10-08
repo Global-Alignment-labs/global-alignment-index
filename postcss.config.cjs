23b99abcc319394119d6012ae3f7ef67ce2709f6
Madoc <165958580+MadocVZ@users.noreply.github.com>
2025-10-08 14:21:15 +0200

    Ensure PostCSS config runs under ESM build

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,1 +1,7 @@
-module.exports = { plugins: { tailwindcss: {}, autoprefixer: {} } }
+// PostCSS must be CommonJS under "type": "module"
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
